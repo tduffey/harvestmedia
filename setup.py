7ea61b520e0a65d0f32604a5a368019e3028ff94
Tom Duffey <tduffey@crescendocollective.com>
2017-07-18 20:04:57 -0500

    Removed cElementTree dependency since it is now included in Python 2.5+ and not available via pip

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
       author_email='ryan@roemmich.org',
       url='https://github.com/ralfonso/harvestmedia',
       description='An interface for the Harvest Media API (http://www.harvestmedia.net/)',
-      install_requires=['httplib2', 'cElementTree', 'pytz', 'iso8601', ],
+      install_requires=['httplib2', 'pytz', 'iso8601', ],
       keywords='Harvest HarvestMedia API Media Music',
 )
