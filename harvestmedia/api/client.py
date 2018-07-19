596ce1e54e542a746040d3e0a45062c566535727
Lucas Belcher <lucas.belcher@gmail.com>
2018-07-19 15:02:27 -0500

    testing nginx proxy

--- harvestmedia/api/client.py
+++ harvestmedia/api/client.py
@@ -23,7 +23,7 @@ class Client(object):
     """
 
     def __init__(self, api_key, debug_level='INFO',
-                    webservice_url='https://service.harvestmedia.net/HMP-WS.svc'):
+                    webservice_url='https://services.harvestmedia.local'):
 
         self.api_key = api_key
         self.debug_level = debug_level
